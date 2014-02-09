5960dd8143998c4e12c7903f8e22dc1438a4bdfb
Aaron Holland <aaronholla@gmail.com>
2014-02-09 06:17:06 -0500

    Added Output to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,29 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+puts "Adding data to the database...\n
+		==================================="
+
+puts "Events being added to database...\n
+		==================================="
+
 jsonevents = ActiveSupport::JSON.decode(File.read('db/seeds/events.json'))
 
 jsonevents.each do |a| 
   Event.create!(:key => a['key'], :name => a['name'], :short_name => a['short_name'], :start_date => a['start_date'], :end_date => a['end_date'], :official => a['official']) 
+  puts "Event added. #{a['name']}"
 end
+
+puts "All Events Added."
+
+puts "Teams being added to database...\n
+		==================================="
 
 jsonteams = ActiveSupport::JSON.decode(File.read('db/seeds/teams.json'))
 
 jsonteams.each do |a| 
   Team.create!(:team_number => a['team_number'], :nickname => a['nickname'], :name => a['name'], :key => a['key'], :website => a['website'], :locality => a['locality'], :region => a['region'], :country_name => a['country_name'], :location => a['location']) 
+  puts "Team added. #{a['team_number']}"
 end
+
+puts "All Teams Added."
