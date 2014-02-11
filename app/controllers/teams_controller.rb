4d85d0b1a24df9ccefce857e1e7854a1d6f22bfc
Aaron Holland <aaronholla@gmail.com>
2014-02-11 14:55:33 -0500

    Added sorting to the teams page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,10 +1,11 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show]
+  helper_method :sort_column, :sort_direction
 
   # GET /teams
   # GET /teams.json
   def index
-    @teams = Team.all
+    @teams = Team.order(sort_column + " " + sort_direction)
   end
 
   # GET /teams/1
@@ -21,5 +22,13 @@ class TeamsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def team_params
       params[:team]
+    end
+
+    def sort_column
+      Team.column_names.include?(params[:sort]) ? params[:sort] : "team_number"
+    end
+    
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
