696dd4ac35e8645e16e5a68c14f042e1720030c0
Aaron Holland <aaronholla@gmail.com>
2014-02-12 09:38:00 -0500

    Updated About page
    Fixed styling and change the github repo on the buttons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,13 @@ Binofparts::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'binofparts.com' }
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => "aaronholla@gmail.com",
+    :password  => "3E_piPzf1L1Ygo2t28yyIg"
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
