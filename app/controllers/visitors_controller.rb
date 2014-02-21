6741bef01e3db9e0a39806771b5a694d977502c8
Aaron Holland <aaronholla@gmail.com>
2014-02-20 23:45:29 -0500

    Added Mailing list and saves form submit to database
    Now checks against the database to see if email already registered.

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -5,13 +5,26 @@ class VisitorsController < ApplicationController
   end
 
   def send_mail
-  	email = params[:send_mail][:email]
-  	first_name = params[:send_mail][:first_name]
-  	last_name = params[:send_mail][:last_name]
-  	team = params[:send_mail][:team]
-  	Notifier.send_mail(email, first_name, last_name, team).deliver
-  	flash[:notice] = "Thank you for registering. When the beta is ready an email will be sent to #{email}."
-  	redirect_to root_path
+    email = params[:send_mail][:email].downcase
+    first_name = params[:send_mail][:first_name]
+    last_name = params[:send_mail][:last_name]
+    team = params[:send_mail][:team]
+    post_exists = MailingList.where(email: email).exists?
+    if post_exists == true
+      flash[:error] = "The email #{email} has already registered."
+      redirect_to root_path
+    else
+      MailingList.create(:email => email, :first_name => first_name, :last_name => last_name, :team => team)
+    	Notifier.send_mail(email, first_name, last_name, team).deliver
+      flash[:notice] = "Thank you for registering. When the beta is ready an email will be sent to #{email}."
+    	redirect_to root_path
+    end
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def mail_params
+      params.require(:send_mail).permit(:email, :first_name, :last_name, :team)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Myapp::Application.routes.draw do
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   root :to => 'visitors#index'
 
   match '/send_mail', to: 'visitors#send_mail', via: 'post'
