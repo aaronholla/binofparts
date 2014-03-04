cd92333ba7144ae55afb21a5a55ea90c3d3a4ed2
Aaron Holland <aaronholla@gmail.com>
2014-03-04 04:22:06 -0500

    Updated relationships

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ActiveRecord::Base
-	belongs_to :team
+	belongs_to :team, :primary_key => 'team_number'
 	belongs_to :event
-	validates_inclusion_of :year, :in => [2013, 2012, 2011, 2010, 2009], :allow_nil => false
+	belongs_to :part
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
 	has_many :users
+	has_many :requests
 
 	def to_param
 	  team_number
