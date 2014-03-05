74d586d81bb1f8c41e53c2b8b70c2925d562b9b3
Aaron Holland <aaronholla@gmail.com>
2014-03-05 00:42:57 -0500

    Added Events to Homepage
    Also added my team to the homepage
    Requests Now only show from the events the team is attending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
+	before_action :set_team, only: [:index]
 	def index
-		@requests = Event.find_by_key('2014txsa').requests
+	    @events = []
+	    @team.events.each do |event|
+	    	@requests = Event.find_by_key(event).requests
+	       	@events << Event.find_by_key(event)
+	    end
+
+	    @myteam = User.where(:team_number_id => current_user.team_number_id)
 	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_team
+      @team = Team.find_by_team_number(current_user.team_number_id)
+    end
+
 end
