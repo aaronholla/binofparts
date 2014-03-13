0e9e542ff362266481d7e02133d76e193ceeec83
Aaron Holland <aaronholla@gmail.com>
2014-03-13 02:31:37 -0400

    Converts qty to number now

--- app/controllers/api/v1/inventory_controller.rb
+++ app/controllers/api/v1/inventory_controller.rb
@@ -13,7 +13,7 @@ class API::V1::InventoriesController < API::V1::ApplicationController
   def new
     @event.requests.create(:accepted => true,:accepted_at=>Time.now, :accepted_by => current_user.team_number_id, :part_id=> params[:part_id], :qty=> params[:qty], :event_id=> params[:event_id], :team_id=> params[:team_id])
     @inventory = @event.inventories.find(params[:inventory_id]) 
-    @inventory.qty = @inventory.qty - params[:qty]
+    @inventory.qty = @inventory.qty - params[:qty].to_f
     @inventory.save
     render :status=>200, :json=>{:message=>"Success"}
   end
