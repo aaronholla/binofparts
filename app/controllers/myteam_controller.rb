5acde272a32a313ead69f6193b004af2251a5e3c
Aaron Holland <aaronholla@gmail.com>
2014-04-09 05:42:58 -0400

    Updated my team page to show admins if your an admin

--- app/controllers/myteam_controller.rb
+++ app/controllers/myteam_controller.rb
@@ -3,6 +3,10 @@ class MyteamController < ApplicationController
 
   # GET /myteams
   def index
+  	if current_user.admin?
+  		@admins = User.where(:admin => true)
+  	end
+  	
     @myteam = User.where(:team_number_id => current_user.team_number_id)
   end
 end
