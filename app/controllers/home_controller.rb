01762a57913593b585cdd8d8267e7ab9d5d879af
Aaron Holland <aaronholla@gmail.com>
2014-04-09 13:55:49 -0400

    Moved event_ids to private function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,12 @@
 class HomeController < ApplicationController
 	before_action :set_team, only: [:index]
+	before_action :get_event_ids
 	def index
-	    @event_ids = []
-	    @events = []
 	    if current_user.admin?
-		    @event = Event.find_by_key("2014flor")
-		    @requests = @event.requests.order("updated_at DESC")
-
+			@requests = Request.where(:event_id => @event_ids).order("updated_at DESC")
 		    # @events.sort!{|a,b|a.start_date <=> b.start_date}
 		    @admins = User.where(:admin => true)
 	    else
-		    @team.events.each_with_index do |event, index|
-		       	@events << Event.find_by_key(event)
-		       	@event_ids << @events[index].id
-		    end
-
 		    @requests = Request.where(:event_id => @event_ids).order("updated_at DESC")
 
 		    @events.sort!{|a,b|a.start_date <=> b.start_date}
@@ -24,7 +16,7 @@ class HomeController < ApplicationController
 
 	def update_feed
 		time_ago = Time.now - 8.seconds
-		@requests = Request.where("updated_at > ?", time_ago).order("updated_at DESC")
+		@requests = Request.where("event_id in (?) and updated_at > ?", @event_ids, time_ago).order("updated_at DESC")
 		  respond_to do |format|
 		      format.js {render 'update_feed.js.erb'}
 		  end
@@ -35,6 +27,23 @@ class HomeController < ApplicationController
     def set_team
     	if not current_user.admin?
     		@team = Team.find_by_team_number(current_user.team_number_id)
+    	end
+    end
+
+    def get_event_ids
+    	@event_ids = []
+	    @events = []
+    	if current_user.admin?
+    		@event_keys = ["2014flor", "2014flfo"]
+	    	@event_keys.each_with_index do |event, index|
+	    		@events << Event.find_by_key(event)
+		       	@event_ids << @events[index].id
+			end
+    	else
+    		@team.events.each_with_index do |event, index|
+		       	@events << Event.find_by_key(event)
+		       	@event_ids << @events[index].id
+		    end
     	end
     end
 
