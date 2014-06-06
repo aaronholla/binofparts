a7f20abd00786a33b1fb39b56a1997fbbbfaef6e
Aaron Holland <aaronholla@gmail.com>
2014-06-05 22:26:28 -0400

    Fixed name error in api spec files
    also added database file for travis
    updated config file for travis again

---
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe API::V1::EventsController, :type => :controller do
+
+end
---
+++ spec/controllers/api/v1/parts_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe API::V1::PartsController, :type => :controller do
+
+end
