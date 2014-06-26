34538050fbfffcab99ecba33fca6e19459471a49
Aaron Holland <aaronholla@gmail.com>
2014-06-26 01:42:59 -0400

    Removed Event spec

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "Events", :type => :request do
-  describe "GET /events" do
-    it "displays events" do
-      FactoryGirl.create(:event, :name => "orlando regional", :key => "2014flor")
-      get events_path
-      expect(response.body).to include("orlando regional")
-    end
-  end
+  
 end
