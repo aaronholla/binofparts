6f030e409561cf8e008e07d52f32f022896096d8
Aaron Holland <aaronholla@gmail.com>
2014-06-26 01:41:23 -0400

    Added Json Spec Helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,7 @@ require 'coveralls'
 Coveralls.wear!
 
 RSpec.configure do |config|
+  #config.include Requests::JsonHelpers, :type => :request
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
