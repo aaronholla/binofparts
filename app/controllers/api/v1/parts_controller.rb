2159ce040293359f7895ad870a883d5a10cc365c
Aaron Holland <aaronholla@gmail.com>
2014-06-26 02:03:32 -0400

    Minor Changes to parts

--- app/controllers/api/v1/parts_controller.rb
+++ app/controllers/api/v1/parts_controller.rb
@@ -1,7 +1,21 @@
 class API::V1::PartsController < API::V1::BaseController
   respond_to :json
 
-  def category
-  	respond_with(Part.where(:category => params[:id]))
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  before_action :set_part, only: [:show]
+
+  # GET /parts/:id
+  def show
+    respond_with(@part, :only => [:name, :description, :number, :qty, :picture, :year])
   end
+
+  private
+    def set_part
+      @part = Part.find(params[:id])
+    end
+
+    def record_not_found
+      render :status=>404, :json=>{:error=>"Part does not exist."}
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Binofparts::Application.routes.draw do
       end
       resources :teams, only: [:index, :show]
       get 'kop/:id' => 'kop#show'
-      get 'parts/:id' => 'parts#category'
+      resources :parts, only: [:index, :show]
     end
   end
 
@@ -24,7 +24,7 @@ Binofparts::Application.routes.draw do
 
   resources :teams, only: %w(index show)
 
-  get 'about' => 'about#index'  
+  get 'about' => 'about#index'
   get 'kop' => 'kop#index'
   get 'kop/:id' => 'kop#show', :as => 'kop_year'
 
