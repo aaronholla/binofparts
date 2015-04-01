b2b30badb477a3d2ae12d9bf9d0f9efb86eab72b
Aaron Holland <aaronholla@gmail.com>
2015-04-01 03:08:10 -0400

    Minor changes to test names

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe AboutController, :type => :controller do
 	describe "GET #index" do
-	    it "should respond successfully with an HTTP 200 status code" do
+	    it "returns a 200 status" do
 	      get :index
 	      expect(response).to be_success
 	      expect(response.status).to eq(200)
 	    end
 
-	    it "should render the index template" do
+	    it "renders the index template" do
 	      get :index
 	      expect(response).to render_template("index")
 	    end
